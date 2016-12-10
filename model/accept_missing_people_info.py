d81d67698336a6e80dcf18fccc5db69cc90b6bb0
EdlinLink <edlinlink@qq.com>
2016-12-10 23:08:24 +0800

    add: get_people_list

--- model/accept_missing_people_info.py
+++ model/accept_missing_people_info.py
@@ -17,8 +17,8 @@ class AcceptMissingPeopleInfo(object):
     def Insert(self):
         #info = JSONDecoder().decode(self.json_req)
         info = self.info
-        sql = "insert into MissingPeople (userid, userphone, userloc, missing_name, missing_age, missing_sex, missing_city, missing_date, missing_desc, photoUrl ) values (\"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\" );"
-        sql = sql % (info["userid"], info["userphone"], info["userloc"], info["missing_name"], info["missing_age"], info["missing_sex"],info["missing_city"], info["missing_date"], info["missing_desc"], info["photoUrl"])
+        sql = "insert into MissingPeople (userid, userphone, userloc, missing_name, missing_age, missing_sex, missing_city, missing_date, missing_desc, photoUrl, local_url ) values (\"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\" );"
+        sql = sql % (info["userid"], info["userphone"], info["userloc"], info["missing_name"], info["missing_age"], info["missing_sex"],info["missing_city"], info["missing_date"], info["missing_desc"], info["photoUrl"], info["local_url"])
 
         self.model.execute( sql )
 	
--- model/get_people_list.py
+++ model/get_people_list.py
@@ -12,14 +12,11 @@ class GetPoepleList(object):
     def __init__(self):
         self.model = BaseDataModel()
 
-    #def GetPeople(self, count):
-    #    sql = "MissingPeople (userid, userphone, userloc, missing_name, missing_age, missing_sex, missing_city, missing_date, missing_desc, photoUrl ) values (\"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\" );"
-    #    sql = sql % (info["userid"], info["userphone"], info["userloc"], info["missing_name"], info["missing_age"], info["missing_sex"],info["missing_city"], info["missing_date"], info["missing_desc"], info["photoUrl"])
-    #    
-    #    self.model.execute( sql )
+    def GetPeople(self, count, offset):
+        sql = "SELECT local_url from MissingPeople LIMIT \"%d\",  \"%d\" );"
+        sql = sql % ( offset, count )
+        return self.model.execute( sql )
 	
-    
-
 
 if __name__=='__main__':
     obj = GetPoepleList(json_req)
