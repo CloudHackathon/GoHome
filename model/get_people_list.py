60365fc08c4a95175be6179f50164c606696b2c0
EdlinLink <edlinlink@qq.com>
2016-12-10 23:53:12 +0800

    add: one field

--- model/get_people_list.py
+++ model/get_people_list.py
@@ -12,8 +12,8 @@ class GetPoepleList(object):
     def __init__(self):
         self.model = BaseDataModel()
 
-    def GetPeople(self, count, offset):
-        sql = "SELECT local_url from MissingPeople LIMIT \"%d\",  \"%d\" );"
+    def GetPeople(self, offset, count ):
+        sql = "SELECT id, local_url, photoUrl from MissingPeople LIMIT \"%d\",  \"%d\" );"
         sql = sql % ( offset, count )
         return self.model.execute( sql )
 	
